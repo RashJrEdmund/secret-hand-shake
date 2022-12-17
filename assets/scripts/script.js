02e63d602d4549b2f071335df40e93550ec0f299
rash <orashusedmund@gmail.com>
2022-12-17 17:37:57 +0100

    correcting linters

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,51 +3,51 @@ const displayResults = document.querySelector('.display-results')
 const shakeHandBtn = document.querySelector('.btn')
 
 const arrayOfCode = [
-  {bin: 1, response: 'wink', deci: 1},
-  {bin: 10, response: 'double blink', deci: 2},
-  {bin: 11, response: 'wink, double blink', deci: 3},
-  {bin: 100, response: 'close your eyes', deci: 4},
-  {bin: 1000, response: 'jump', deci: 8},
+  { bin: 1, response: 'wink', deci: 1 },
+  { bin: 10, response: 'double blink', deci: 2 },
+  { bin: 11, response: 'wink, double blink', deci: 3 },
+  { bin: 100, response: 'close your eyes', deci: 4 },
+  { bin: 1000, response: 'jump', deci: 8 },
 ]
 
 const reversedArray = [
-  {bin:1000, response: 'jump', deci: 8},
-  {bin: 100, response: 'close your eyes', deci: 4},
-  {bin: 11, response: 'double blink, wink', deci: 3},
-  {bin: 10, response: 'double blink', deci: 2},
-  {bin: 1, response: 'wink', deci: 1},
+  { bin:1000, response: 'jump', deci: 8 },
+  { bin: 100, response: 'close your eyes', deci: 4 },
+  { bin: 11, response: 'double blink, wink', deci: 3 },
+  { bin: 10, response: 'double blink', deci: 2 },
+  { bin: 1, response: 'wink', deci: 1 }
 ]
 
-let k=0; //k counts the number of times findResponse is called and chooses to add a comma and space
+let k=0 // k counts the number of times findResponse is called and chooses to add a comma and space
 let revert = false
 
 const convertToBinary = (val) => {
-  let remainder = 0, binary = 0, i=1;
-  while(val!==0) {
+  let remainder = 0, binary = 0, i = 1
+  while( val !== 0) {
     remainder = val%2
-    val = parseInt(val/2);
-    binary += remainder*i
-    i *= 10;
+    val = parseInt(val/2)
+    binary += remainder * i
+    i *= 10
   }
   return binary
 }
 
 const findResponse = (val, arr) => {
-  let binVal = convertToBinary(val)
+  const binVal = convertToBinary(val)
   console.log(binVal)
-  for (let i=0; i<arr.length; i++) {
-    if(binVal === arr[i].bin){ //calling the fxn to convert to binary
-      displayResults.innerHTML += `${k > 0? ', ':''}${arr[i].response}`
+  for (let i = 0; i < arr.length; i++) {
+    if (binVal === arr[i].bin) { //calling the fxn to convert to binary
+      displayResults.innerHTML += `${k > 0 ? ', ' : ''}${arr[i].response}`
     }
   }
 }
 
 const checkRange = (valu, arre) => {
-  if ((valu <= 4)||(valu === 8)) {
+  if ((valu <= 4) || (valu === 8)) {
     findResponse(valu, arre)
   }
-  else if((valu > 4)&&(valu < 8)) {
-    if(revert === false){
+  else if ((valu > 4)&&(valu < 8)) {
+    if (revert === false) {
       findResponse(valu-4, arre)
       k++
       findResponse(4, arre)
@@ -55,36 +55,36 @@ const checkRange = (valu, arre) => {
     else {
       findResponse(4, arre)
       k++
-      findResponse(valu-4, arre)
+      findResponse(valu - 4, arre)
     }
   }
-  else if((valu > 8)&&(valu < 16)) {
-    if(valu < 13) {
+  else if ((valu > 8)&&(valu < 16)) {
+    if (valu < 13) {
       if (revert === false) {
-        findResponse(valu-8, arre)
+        findResponse(valu - 8, arre)
         k++
         findResponse(8, arre)
       }
       else {
         findResponse(8, arre)
         k++
-        findResponse(valu-8, arre)
+        findResponse(valu - 8, arre)
       }
     }
     else {
-      if(revert === false) {
-        findResponse(valu-12, arre)
+      if (revert === false) {
+        findResponse(valu - 12, arre)
         k++
-        findResponse((valu-(valu-12))-8, arre)
+        findResponse((valu - (valu - 12)) - 8, arre)
         k++
         findResponse(8, arre)
       }
       else {
         findResponse(8, arre)
         k++
-        findResponse((valu-(valu-12))-8, arre)
+        findResponse((valu - (valu - 12)) - 8, arre)
         k++
-        findResponse(valu-12, arre)
+        findResponse(valu - 12, arre)
       }
     }
   }
@@ -93,16 +93,15 @@ const checkRange = (valu, arre) => {
 
 shakeHandBtn.addEventListener('click', ()=> {
   displayResults.innerHTML = ''
-  let resultant = 0
   if (inputNumber.value < 16) {
     checkRange(inputNumber.value, arrayOfCode)
   }
   else if (inputNumber.value >= 16) {
-    if(inputNumber.value === 16) {
+    if (inputNumber.value === 16) {
       displayResults.innerHTML += `! secret Code reversed`
     } else {
       revert = true
-      checkRange((inputNumber.value-16), reversedArray)
+      checkRange((inputNumber.value - 16), reversedArray)
       console.log(revert)
     }
   }
